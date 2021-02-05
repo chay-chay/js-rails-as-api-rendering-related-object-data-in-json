5e620f2c2649d8c615a026713cff031fcd9d552b
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-02-05 16:18:39 -0500

    Done.

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,28 @@
+class SightingsController < ApplicationController
+
+    def index
+        sightings = Sighting.all
+        render json: sightings.to_json(include: [:bird, :location])
+      end
+
+      def show
+        sighting = Sighting.find_by(id: params[:id])
+        render json: sighting.to_json(:include => {
+          :bird => {:only => [:name, :species]},
+          :location => {:only => [:latitude, :longitude]}
+        }, :except => [:updated_at])
+      end 
+
+    #   def show
+    #     sighting = Sighting.find_by(id: params[:id])
+    #     if sighting
+    #       render json: sighting.to_json(include: [:bird, :location])
+    #     else
+    #       render json: { message: 'No sighting found with that id' }
+    #     end
+        # render json: sighting, include: [:bird, :location]
+
+        # An alternative is to use the include option to indicate what models you want to nest: 
+        # render json: { id: sighting.id, bird: sighting.bird, location: sighting.location }
+    #   end 
+end
